aed91e8ba1cd9bf149717f33e88a22d39d079b4d
bright <yuanzhouhui@gmail.com>
2021-01-22 17:10:04 +0800

    服务熔断

--- cloud-provider-hystrix-payment8001/src/main/java/com/bright/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/bright/springcloud/controller/PaymentController.java
@@ -37,4 +37,16 @@ public class PaymentController {
 		log.info("****result: " + result);
 		return result;
 	}
+
+	/**
+	 * 服务熔断
+	 * @param id
+	 * @return
+	 */
+	@GetMapping(value = "/payment/circuit/{id}")
+	public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+		String result = paymentService.paymentCircuitBreaker(id);
+		log.info("***result: " + result);
+		return result;
+	}
 }
