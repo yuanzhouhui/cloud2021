7cecdd38e286dcc6eea50556a4c11af5613d74dd
bright <yuanzhouhui@gmail.com>
2021-01-22 15:50:47 +0800

    80客户端服务降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/bright/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/bright/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,8 @@
 package com.bright.springcloud.controller;
 
 import com.bright.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,8 +30,16 @@ public class OrderHystrixController {
 	}
 
 	@GetMapping(value = "/consumer/payment/hystrix/timeout/{id}")
+	@HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+	})
 	String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+		//int age=10/0;
 		String result = paymentHystrixService.paymentInfo_TimeOut(id);
 		return result;
+	}
+
+	String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id){
+		return "我是消费者80,对方支付系统繁忙,请稍后再试或自身运行出错,o(╥﹏╥)o";
 	}
 }
--- cloud-consumer-feign-hystrix-order80/src/main/java/com/bright/springcloud/service/PaymentHystrixService.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/bright/springcloud/service/PaymentHystrixService.java
@@ -1,5 +1,7 @@
 package com.bright.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
