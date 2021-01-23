292fd802d6681442fdae1b37d1b428aba2d2e37a
bright <yuanzhouhui@gmail.com>
2021-01-23 17:06:14 +0800

    新建config-center-3344工程,配置服务端配置中心

---
+++ cloud-config-center-3344/src/main/java/com/bright/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,21 @@
+package com.bright.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author bright
+ * @version 1.0
+ * @description
+ * @date 2021-01-23 16:10
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigCenterMain3344.class, args);
+	}
+}
