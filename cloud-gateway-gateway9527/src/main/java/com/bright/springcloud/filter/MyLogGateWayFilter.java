a0ed061669cbea5177ff36c333849210a123b480
bright <yuanzhouhui@gmail.com>
2021-01-23 15:48:01 +0800

    gateway 自定义全局filter

---
+++ cloud-gateway-gateway9527/src/main/java/com/bright/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,41 @@
+package com.bright.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @author bright
+ * @version 1.0
+ * @description
+ * @date 2021-01-23 15:39
+ */
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		log.info("**********come in MyLogGateWayFilter: " + new Date());
+		String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+		if (StringUtils.isEmpty(uname)) {
+			log.info("**********用户名为空,非法用户,o(╥﹏╥)o");
+			exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+			return exchange.getResponse().setComplete();
+		}
+		return chain.filter(exchange);
+	}
+
+	@Override
+	public int getOrder() {
+		return 0;
+	}
+}
