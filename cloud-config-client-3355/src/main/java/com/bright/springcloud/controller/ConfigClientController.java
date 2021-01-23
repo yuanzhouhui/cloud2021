6388e6ac6fc19661e0dc2ea0e7fc7c7d2a48ad22
bright <yuanzhouhui@gmail.com>
2021-01-23 17:40:20 +0800

    3355客户端动态获取3344配置信息(发送post请求refresh->3355实现动态刷新)

--- cloud-config-client-3355/src/main/java/com/bright/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/bright/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.bright.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021-01-23 17:18
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
 	@Value("${config.info}")
