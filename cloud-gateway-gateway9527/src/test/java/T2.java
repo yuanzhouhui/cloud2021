101f9eb591139c7eac1c0eaa958ffd6aa9d184f1
bright <yuanzhouhui@gmail.com>
2021-01-23 15:31:13 +0800

    gateway predicate配置

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author bright
+ * @version 1.0
+ * @description
+ * @date 2021-01-23 15:11
+ */
+public class T2 {
+	@Test
+	public static void main(String[] args) {
+		ZonedDateTime zdt = ZonedDateTime.now();
+		System.out.println(zdt);
+	}
+}
