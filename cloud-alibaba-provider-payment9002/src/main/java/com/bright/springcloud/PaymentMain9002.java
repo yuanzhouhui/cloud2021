549006439c0dc9e75005248230fa2665ccfc26db
bright <yuanzhouhui@gmail.com>
2021-01-26 17:25:50 +0800

    新建nacos9002工程,将该服务注册进nacos-server

---
+++ cloud-alibaba-provider-payment9002/src/main/java/com/bright/springcloud/PaymentMain9002.java
@@ -0,0 +1,19 @@
+package com.bright.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author bright
+ * @version 1.0
+ * @description
+ * @date 2021-01-26 17:23
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentMain9002.class, args);
+	}
+}
---
+++ cloud-alibaba-provider-payment9002/src/main/java/com/bright/springcloud/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.bright.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author bright
+ * @version 1.0
+ * @description
+ * @date 2021-01-26 17:23
+ */
+@RestController
+public class PaymentController {
+
+	@Value("${server.port}")
+	private String serverPort;
+
+	@GetMapping(value = "/payment/nacos/{id}")
+	public String getPayment(@PathVariable("id") Integer id) {
+		return "nacos registry, serverPort: " + serverPort + "\t ,id: " + id;
+	}
+}
