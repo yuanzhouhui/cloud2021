15fdc51f26cfaebf228398f625cb4f41fc5cd574
bright <yuanzhouhui@gmail.com>
2021-01-28 18:29:06 +0800

    测试sentinel热点参数降级

--- cloud-alibaba-sentinel-service8401/src/main/java/com/bright/springcloud/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/com/bright/springcloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.bright.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -45,5 +48,16 @@ public class FlowLimitController {
 		log.info("testE 测试异常数");
 		int age = 10 / 0;
 		return "-----------E";
+	}
+
+	@GetMapping(value = "/testHotKey")
+	@SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+	public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+							 @RequestParam(value = "p2", required = false) String p2) {
+		return "------testHotKey";
+	}
+
+	public String deal_testHotKey(String p1, String p2, BlockException exception) {
+		return "------deal_testHotKey,o(╥﹏╥)o";
 	}
 }
